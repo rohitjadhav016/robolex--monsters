9003f38d387dd632eb0c535f6e4481fa186ea51d
rohitjadhav016 <jrohit016@gmail.com>
2023-05-20 19:10:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monster : [],
+      searchField : ''
+    }
+    console.log('constructor');
+  }
+
+  searchFieldChange = (event) =>{    
+  const searchField = event.target.value.toLocaleLowerCase();
+  this.setState(()=>{
+    return {searchField}
+  })
+
+  }
+
+
+  componentDidMount(){
+    console.log('componentDidMount');
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) =>  response.json())
+      .then((users) => { this.setState(() => {
+        return {monster: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+       )})
+  }
+
+  render(){
+    console.log('render');
+    const { monster, searchField } = this.state;
+    const { searchFieldChange } = this;
+    const filteredMonsters = monster.filter((mon) => {
+      return mon.name.toLocaleLowerCase().includes(searchField);
+    })
+    return (
+      <div className="App">
+        <input type='search' className='search-box' placeholder='search monsters' 
+        onChange={searchFieldChange}/>
+        {
+          filteredMonsters.map((mon) => {
+            return(
+              <div key={mon.id}>
+                  <h1> {mon.name} </h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
+  
 }
 
 export default App;
